8450458abfdb124fb8af29683ea5e68e0c25b675
Berry  Semexan <bsemexan@gmail.com>
2018-04-21 23:20:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function doToElementsInArray(evens,callback){
 
 // Add your changeCompletely() function here:
 function changeCompletely(elem, index, arr){
-  arr[index] = `${elem} !`
+  arr[index] = `${elem}!`
 }
